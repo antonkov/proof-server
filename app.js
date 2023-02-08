94f4fa6a663565a4987e478bd7d817c4ee329419
Anton Kovsharov <antonkovsharov@gmail.com>
2023-02-08 15:54:41 +0800

    Ignore CORS and add version id

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express()
 const port = 3000
+
+const allowedOrigins = ['localhost:5431']
+app.use(cors({
+  origin: function (origin, callback) {
+    return callback(null, true);
+    /*if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);*/
+  }
+
+}));
 
 app.use(bodyParser.json());
 
@@ -10,10 +25,11 @@ app.get('/', (req, res) => {
 })
 
 let hyps = [];
+let curId = 1;
 
 app.post('/sendTypes', (req, res) => {
   const data = req.body;
-  hyps = data;
+  hyps = { data, id: curId++ };
   console.log('Receved', data);
   res.send(`Receved ${data}`);
 });
